8dc4f92154cb47cf1ce9b30762030807ac71dc18
George Meisinger <gmeisinger@Gmail.com>
2025-10-08 14:42:19 -0500

    - Fixing links. Resume has a project page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://gmeisinger.github.io',
-  base: 'gmeisinger.github.io'
-});
+export default defineConfig({});
